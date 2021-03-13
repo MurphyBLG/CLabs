433f188d0a0c46b09842e2b3eb447cb8885a168d
MurphyBLG <erwin16@ya.ru>
2021-03-13 17:15:25 +0500

    One more update

--- lab2/2.c
+++ lab2/2.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
 #include <limits.h>
 
+int maxForwardIdx (int a[], int size) {
+    int max = a[0], idx = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (a[i] > max) {
+            max = a[i];
+            idx = i;
+        } 
+    }
+    
+    return idx;
+}
+
+int maxBackwardIdx (int a[], int size) {
+    int max = a[0], idx = 0;
+
+    for (int i = size - 1; i >= 0; i--) {
+        if (a[i] > max) {
+            max = a[i];
+            idx = i;
+        } 
+    }
+    
+    return idx;
+}
+
 int main() {
     int n;
     scanf("%d", &n);
     int a[n];
     for (int i = 0; i < n; i++) scanf("%d", &a[i]);
 
-    int firstMax = a[0], secondMax = a[0], firstMaxIdx = -1, secondMaxIdx = -1;
-    int firstMin = a[0], secondMin = a[0], firstMinIdx = -1, secondMinIdx = -1;
+    int  firstMaxIdx = maxForwardIdx(a, n), lastMaxIdx = maxBackwardIdx(a, n);
+    int firstMin = a[0], lastMin = a[0];
     for (int i = 0; i < n; i++) {
-        if (a[i] >= firstMax) {
-            secondMax = firstMax;
-            firstMax = a[i];
-
-            secondMaxIdx = firstMaxIdx;
-            firstMaxIdx = i;
-        }
-
         if (a[i] <= firstMin) {
-            secondMin = firstMin;
+            lastMin = firstMin;
             firstMin = a[i];
-
-            secondMinIdx = firstMinIdx;
-            firstMinIdx = i;
         }
     }
 
-    if ((firstMaxIdx + secondMaxIdx) % 2 == 0) {
-        a[(firstMaxIdx + secondMaxIdx) / 2] = firstMin * secondMin;
+    if ((firstMaxIdx + lastMaxIdx) % 2 == 0) {
+        a[(firstMaxIdx + lastMaxIdx) / 2] = firstMin * lastMin;
     } else {
-        a[(firstMaxIdx + secondMaxIdx) / 2] = firstMin * secondMin;
-        a[(firstMaxIdx + secondMaxIdx) / 2 + 1] = firstMin * secondMin;
+        a[(firstMaxIdx + lastMaxIdx) / 2] = firstMin * lastMin;
+        a[(firstMaxIdx + lastMaxIdx) / 2 + 1] = firstMin * lastMin;
     }
 
     for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
-    printf("\n");
 }
--- lab2/3.c
+++ lab2/3.c
@@ -1,53 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void findAndDel (int arr[], int arr_size) {
-    int pos = -1;
-    for (int i = 1; i < arr_size - 1; i++) {
+void shiftToTheRight (int *arr, int from, int *to) {
+    *to += 1;
+    for (int j = *to - 1; j >= from; j--) {
+        arr[j] = arr[j - 1];
+    }
+}
+
+void shiftToTheLeft (int *arr, int from, int *to) {
+    *to -= 1;
+    for (int j = from; j < *to; j++) {
+        arr[j] = arr[j + 1];
+    }
+}
+
+void findAndDel (int *arr, int *arr_size) {
+    for (int i = 1; i < *arr_size - 1; i++) {
         if (arr[i - 1] % 2 == 0 && arr[i + 1] % 2 == 0 && arr[i] % 2 == 1) {
-            pos = i;
+            shiftToTheLeft(arr, i, arr_size);
             break;
         }
     }
-
-    if (pos != -1) {
-        for (int i = pos; i < arr_size - 1; i++) arr[i] = arr[i + 1];
-    }
 }
 
-void insertZeros (int arr[], int arr_size, int k, int newArr[]) {
-    int pos = 0;
-    for (int i = 0; i < arr_size; i++) {
+
+void insertZeros (int *arr, int *arr_size, int k) {
+    for (int i = 0; i < *arr_size; i++) {
         if (arr[i] == k) {
-            newArr[pos] = 0;
-            newArr[pos + 1] = arr[i];
-            newArr[pos + 2] = 0;
-            pos += 3;
-        } else {
-            newArr[pos] = arr[i];
-            pos++;
+            shiftToTheRight(arr, i, arr_size);
+            arr[i] = 0;
+            i += 2;
+            shiftToTheRight(arr, i, arr_size);
+            arr[i] = 0;
         }
     }
+} 
+
+void printArray(int *arr, int size) {
+    for (int i = 0; i < size; i++) printf("%d ", *(arr + i));
 }
 
 int main() {
-    int n, k;
-    scanf("%d%d", &n, &k);
-    int a[n];
-    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-
-    int kCount = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i] == k) kCount++;
+    int size, k;
+    scanf("%d%d", &size, &k);
+    int *arr;
+    arr = (int*) calloc(size * 2, sizeof(int));
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
-
-    int newSize = n + kCount * 2;
-    int new[newSize];
     
-    insertZeros(a, newSize, k, new);
+    insertZeros(arr, &size, k);
 
-    findAndDel(new, newSize);
+    findAndDel(arr, &size);
 
-    for (int i = 0; i < newSize - 1; i++) printf("%d ", new[i]);
-
-    printf("\n");
+    printArray(arr, size);
 }
