632b98030f43eb93cb214116f04e4c82a5dcf069
MurphyBLG <erwin16@ya.ru>
2021-03-18 10:36:10 +0500

    fix while

--- lab4/2.c
+++ lab4/2.c
@@ -73,16 +73,28 @@ int main() {
                 continue;
             }
 
-            if (last == NULL && strcmp(word, first->info.word) > 0) {
+            if (last == NULL) {
                 last = (struct node*) malloc(sizeof(struct node));
 
-                last->next = NULL;
-                last->prev = first;
-                first->next = last;
+                if (strcmp(word, first->info.word) >= 0) {
+                    last->next = NULL;
+                    last->prev = first;
+                    first->next = last;
 
-                memoryAllocation(last, word, translation, example);
-                system("clear");
-                continue;
+                    memoryAllocation(last, word, translation, example);
+                    system("clear");
+                    continue;
+                } else {
+                    memoryAllocation(last, first->info.word, first->info.translation, first->info.example);
+                    memoryAllocation(first, word, translation, example);
+
+                    last->prev = first;
+                    last->next = NULL;
+                    first->next = last;
+
+                    system("clear");
+                    continue;
+                }
             }
 
             struct node *tmp = malloc(sizeof(struct node));
@@ -105,18 +117,42 @@ int main() {
                 itr = itr->next;
             }
 
-            system("clear");
+            itr = last;
+            while (itr != NULL)
+            {
+                if (strcmp(tmp->info.word, itr->info.word) >= 0) {
+                    tmp->prev = itr;
+                    tmp->next = itr->next;
+                    printf("%s %s %s\n", tmp->next->info.word, tmp->prev->info.word, itr->next->info.word);
+                    if (itr->next == NULL) {
+                        last = tmp;
+                    } else {
+                        itr->next->prev = tmp;
+                    }
+                    itr->next = tmp;
+                    
+                    break;
+                } 
+
+                itr = itr->prev;
+            }
+            itr = first;
+            printf("%s %s %s", itr->info.word, itr->next->info.word, itr->next->next->info);
+            
         } else break;
     }
     
     /*printf("Enter the word which translation you want to find: ");
     char request[256];
     scanf("%s", request);*/
-    struct node *itr = first;
-    while (itr != NULL)
-    {
-        printf("%s\n", itr->info.word);
-        itr = itr->next;
-    }
+    
 
 }
+/* last->prev = NULL;
+                    last->next = first;
+                    first->prev = last;
+                    memoryAllocation(last, first->info.word, first->info.translation, first->info.example);
+                    struct dictionary new = first->info;
+                    first->info = last->info;
+                    last->info = new;
+                    */
