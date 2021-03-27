5cfa01c564552d48e59937952b81a6ed581de625
MurphyBLG <erwin16@ya.ru>
2021-03-27 12:24:42 +0500

    bin search + pairs

--- lab4/1.c
+++ lab4/1.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define system(clear) system("cls")
+
 struct dictionary
 {
     char *word, *translation, *example;
@@ -15,15 +17,24 @@ int cmp (const void  *a, const void *b) {
 }
 
 void printInfo (struct dictionary arr[], int size, char *request) {
-    int found = 0;
-    for (int i = 0; i < size; i++) {
-        if (strcmp(request, arr[i].word) == 0) {
-            printf("Translation: %s\n", arr[i].translation);
-            found = 1;
-        }
+    int L = 0, R = size + 1;
+    while (R - L > 1)
+    {
+        int m = (L + R) / 2;
+        if (arr[m].word > request) R = m;
+        else L = m;
     }
+    
+    if (arr[L].word == request) printf("Translation: %s\n", arr[L].translation);
+    else printf("There is no translation for this word\n");
+}
 
-    if (found == 0) printf("There is no translation for this word\n");
+int isIn (char buffer[], struct dictionary arr[], int size) {
+    for (int j = 0; j < size; j++) {
+            if (strcmp(buffer, arr[j].word) == 0) return j + 1;
+        }
+    
+    return 0;
 }
 
 int main() {
@@ -39,38 +50,60 @@ int main() {
         system("clear");
 
         if (strcmp(buffer, "y") == 0) {
-        size++;
         printf("Enter the word: ");    
         scanf("%s", buffer);
-        arr[i].word = calloc(strlen(buffer + 1), sizeof(char));
-        strcpy(arr[i].word, buffer);
 
-        printf("Enter the translation: ");    
-        scanf("%s", buffer);
-        arr[i].translation = calloc(strlen(buffer + 1), sizeof(char));
-        strcpy(arr[i].translation, buffer);
+        if (isIn(buffer, arr, i)) {
+            int flag = isIn(buffer, arr, i) - 1;
+            printf("Enter the translation: ");    
+            scanf("%s", buffer);
+            arr[flag].translation = realloc(arr[flag].translation, sizeof(arr[flag].translation)+2+sizeof(buffer));
+            strcat(arr[flag].translation, ", ");
+            strcat(arr[flag].translation, buffer);
 
-        printf("Enter the example: ");    
-        scanf("%s", buffer);
-        arr[i].example = calloc(strlen(buffer + 1), sizeof(char));
-        strcpy(arr[i].example, buffer);
+            printf("Enter the example: ");    
+            scanf("%s", buffer);
+            arr[flag].example = realloc(arr[flag].example, sizeof(arr[flag].example)+2);
+            strcat(arr[flag].example, ", ");
+            strcat(arr[flag].example, buffer);
+            i--;
+        } else {
+            size++;
+            arr[i].word = calloc(strlen(buffer + 1), sizeof(char));
+            strcpy(arr[i].word, buffer);
+
+            printf("Enter the translation: ");    
+            scanf("%s", buffer);
+            arr[i].translation = calloc(strlen(buffer + 1), sizeof(char));
+            strcpy(arr[i].translation, buffer);
+
+            printf("Enter the example: ");    
+            scanf("%s", buffer);
+            arr[i].example = calloc(strlen(buffer + 1), sizeof(char));
+            strcpy(arr[i].example, buffer);
+        }
 
         system("clear");
         } else break;
     }
     
-    qsort(arr, size, sizeof(struct dictionary), cmp);
+    //qsort(arr, size, sizeof(struct dictionary), cmp);
 
-    printf("Enter the word which translation you want to find: ");
+    printf("%d", size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%s\n%s\n%s\n\n", arr[i].word, arr[i].translation, arr[i].example);
+    }
+
+    /* printf("Enter the word which translation you want to find: ");
     char request[256];
     scanf("%s", request);
 
-    printInfo(arr, size, request);
+    printInfo(arr, size, request);*/
     
     for (int i = 0; i < size; i++) {
         free(arr[i].word);
         free(arr[i].translation);
         free(arr[i].example);
-    }
-    free(arr);
+    } 
 }
